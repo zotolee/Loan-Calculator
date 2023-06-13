64dcadc1b6db6cb6189122db7f83571e555c15c7
zotolee <akinayo711@gmail.com>
2023-06-13 10:40:41 +0100

    new commit

---
+++ app.js
@@ -0,0 +1,74 @@
+// Listen for submit
+document.getElementById('loan-form').addEventListener('submit', (e) => {
+  e.preventDefault();
+// hide results
+document.getElementById('results').style.display = 'none';
+// show loader
+document.getElementById('loading').style.display = 'block';
+
+setTimeout(calculateResults, 2000);
+
+});
+// Calculate Results
+calculateResults = (e) => {
+  // UI Vars
+  const amount = document.getElementById('amount');
+  const interest = document.getElementById('interest');
+  const years = document.getElementById('years');
+  const monthlyPayment = document.getElementById('monthly-payment');
+  const totalPayment = document.getElementById('total-payment');
+  const totalInterest = document.getElementById('total-interest');
+
+  const principal = parseFloat(amount.value);
+  const calculatedInterest = parseFloat(interest.value) / 100 / 12;
+  const calculatedPayments = parseFloat(years.value) * 12;
+
+  // Compute monthly payment
+  const x = Math.pow(1 + calculatedInterest, calculatedPayments);
+  const monthly = (principal * x * calculatedInterest) / (x-1);
+
+  if(isFinite(monthly)) {
+    monthlyPayment.value = monthly.toFixed(2);  //toFixed is used to round up numbers, in this case 2decimal places.
+    totalPayment.value = (monthly * calculatedPayments).toFixed(2);
+    totalInterest.value = ((monthly * calculatedPayments) - principal).toFixed(2);
+
+    // show results
+document.getElementById('results').style.display = 'block';
+   // hide loader
+document.getElementById('loading').style.display = 'none';
+  } 
+  else {
+    showError('Please check your numbers');
+  }
+}
+// Show Error
+showError = (error) => {
+  // hide results
+document.getElementById('results').style.display = 'none';
+ // hide loader
+document.getElementById('loading').style.display = 'none';
+
+  // Create a div
+  const errorDiv = document.createElement('div');
+
+  // Get elements
+  const card = document.querySelector('.card');
+  const heading = document.querySelector('.heading');
+
+  // Add class
+  errorDiv.className = 'alert alert-danger';
+
+  // Create text node and append to div
+  errorDiv.appendChild(document.createTextNode(error));
+
+  // Insert error above heading
+  card.insertBefore(errorDiv, heading);
+
+  // Clear error after 3 seconds
+  setTimeout(clearError, 3000);
+}
+
+// Clear error
+clearError = () => {
+  document.querySelector('.alert').remove();
+}
